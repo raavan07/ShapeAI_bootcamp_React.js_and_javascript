4794e1462b9863fcd22ccc689dddfc227915ef02
raavan07 <83591131+raavan07@users.noreply.github.com>
2021-06-27 00:25:13 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Info(){
+  return (
+    <div className="note">
+   <h1>Web development using React.js</h1>
+   <p>Basic front end Web development using React.js</p>
+   </div>
+   
+  )
+}
+
+export default Info;
---
+++ Info1.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Info1(){
+  return (
+    <div className="note">
+   <h1>Python and  Cyber security</h1>
+   <p>Basics of Python and Cyber security</p>
+   </div>
+   
+  )
+}
+
+export default Info1;
